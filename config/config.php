7ac672e63e3f010554af893ac642b0f0a7fc985a
Hidroxiuro <60795201+Hidroxiuro@users.noreply.github.com>
2020-05-13 23:54:48 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,15 @@ session_start();
 
 $timezone = date_default_timezone_set("Europe/London");
 
-$con = mysqli_connect("us-cdbr-east-06.cleardb.net", "b00ef6920455e2", "0fa25432", "heroku_2fe7c99a07f0abd"); //Connection variable
+$url = parse_url(getenv("mysql://b00ef6920455e2:0fa25432@us-cdbr-east-06.cleardb.net/heroku_2fe7c99a07f0abd?reconnect=true"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$con = mysqli_connect($server, $username, $password, $db);
+//$con = mysqli_connect("us-cdbr-east-06.cleardb.net", "b00ef6920455e2", "0fa25432", "heroku_2fe7c99a07f0abd"); //Connection variable
 
 if(mysqli_connect_errno()) 
 {
