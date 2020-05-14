86eb82f7c41c09e20c0f7fe757144f2d4923ca1d
Hidroxiuro <60795201+Hidroxiuro@users.noreply.github.com>
2020-05-14 00:26:05 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -14,9 +14,9 @@ $db = substr($url["path"], 1);
 $con = mysqli($server, $username, $password, $db);
 //$con = mysqli_connect("us-cdbr-east-06.cleardb.net", "b00ef6920455e2", "0fa25432", "heroku_2fe7c99a07f0abd"); //Connection variable
 
-if(mysqli_connect_errno()) 
+if(mysqli_errno()) 
 {
-	echo "Failed to connect: " . mysqli_connect_errno();
+	echo "Failed to connect: " . mysqli_errno();
 }
 
 ?>
