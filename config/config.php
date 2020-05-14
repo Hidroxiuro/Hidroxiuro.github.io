617e50684789e95ad7d7ce9b938672a0efa0832a
Hidroxiuro <60795201+Hidroxiuro@users.noreply.github.com>
2020-05-13 23:21:22 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ session_start();
 
 $timezone = date_default_timezone_set("Europe/London");
 
-$con = mysqli_connect("localhost", "root", "", "social"); //Connection variable
+$con = mysqli_connect("@us-cdbr-east-06.cleardb.net", "b00ef6920455e2", "0fa25432", "`heroku_2fe7c99a07f0abd`"); //Connection variable
 
 if(mysqli_connect_errno()) 
 {
